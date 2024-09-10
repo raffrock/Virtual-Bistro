c8c9c622c23fbf54d8e4b465f649d5b6c480b57b
raffRock <102319455+raffrock@users.noreply.github.com>
2024-09-10 00:24:31 -0400

    Trying to make enum wokr

--- test.cpp
+++ test.cpp
@@ -12,6 +12,10 @@
 #include "Dish.cpp"
 
 int main() {
+    // adding enum to file to allow to be in the para of the para'ized constructor
+    enum CuisineType { ITALIAN, MEXICAN, CHINESE, INDIAN,
+AMERICAN, FRENCH, OTHER };
+    
     // creating and testing Dish1 object
     Dish Dish1; // note to see: when using default constructor no ()
     Dish1.setPrepTime(30);
